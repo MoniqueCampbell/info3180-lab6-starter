adbc90eae60fcdd4d9f79eff7fc6557085f157f5
MoniqueCampbell <43890502+MoniqueCampbell@users.noreply.github.com>
2020-04-21 09:12:16 -0500

    lab6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -60,7 +60,7 @@ const NewsList = Vue.component('news-list', {
     `,
     created: function() {
      let self = this;
-     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=75c781b00c4b455595a9c0092649e37f').then(function(response) {
+     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=your-api-key').then(function(response) {
       return response.json();
      }).then(function(data) {
       console.log(data);
